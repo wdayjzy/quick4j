ce81cec489afc19de112ecd35058c7de109eba95
StarZou <zoutongyuan@163.com>
2015-04-22 10:52:02 +0800

    date

---
+++ src/test/java/com/eliteams/quick4j/test/java/api/MyDate.java
@@ -0,0 +1,65 @@
+package com.eliteams.quick4j.test.java.api;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * MyDate : Converting ISO 8601-compliant String to java.util.Date
+ * <p/>
+ * ˆ @author StarZou
+ *
+ * @since 2015-04-22 10:31
+ */
+public class MyDate {
+    @Test
+    public void testConvertDate() throws Exception {
+        System.out.println(ISO8601.toCalendar("2015-03-31T16:00:00.000Z"));
+    }
+}
+
+/**
+ * Helper class for handling a most common subset of ISO 8601 strings
+ * (in the following format: "2008-03-01T13:00:00+01:00"). It supports
+ * parsing the "Z" timezone, but many other less-used features are
+ * missing.
+ */
+final class ISO8601 {
+    /**
+     * Transform Calendar to ISO 8601 string.
+     */
+    public static String fromCalendar(final Calendar calendar) {
+        Date date = calendar.getTime();
+        String formatted = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .format(date);
+        return formatted.substring(0, 22) + ":" + formatted.substring(22);
+    }
+
+    /**
+     * Get current date and time formatted as ISO 8601 string.
+     */
+    public static String now() {
+        return fromCalendar(GregorianCalendar.getInstance());
+    }
+
+    /**
+     * Transform ISO 8601 string to Calendar.
+     */
+    public static Calendar toCalendar(final String iso8601string)
+            throws ParseException {
+        Calendar calendar = GregorianCalendar.getInstance();
+        String s = iso8601string.replace("Z", "+00:00");
+        try {
+            s = s.substring(0, 22) + s.substring(23);  // to get rid of the ":"
+        } catch (IndexOutOfBoundsException e) {
+            throw new ParseException("Invalid length", 0);
+        }
+        Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(s);
+        calendar.setTime(date);
+        return calendar;
+    }
+}
